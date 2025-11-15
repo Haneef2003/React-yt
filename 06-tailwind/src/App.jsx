400fc126fc95a9fe42294c4ccaafaf5e1f26a9bd
Mohammad Haneef <haneefmd884@gmail.com>
2025-11-15 16:41:03 +0530

    Sorry I pushed folder Seven earlier and now I pushed everything

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500'>
+    <div className='h-30 w-100% bg-pink-400'></div>
+    </div>
+  )
+}
+
+export default App
